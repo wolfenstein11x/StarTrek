924f798e74ee3b2b4bcd9d94d18b4a73eb56691a
Charles <c.wolfe132@gmail.com>
2020-12-04 19:47:52 -0800

    player shoots now

---
+++ StarTrek/Assets/Scripts/ShipShooting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipShooting : MonoBehaviour
+{
+
+    [Header("Projectiles")]
+    [SerializeField] GameObject torpedoPrefab;
+    
+    [Header("Sound Effects")]
+    [SerializeField] AudioClip torpedoSound;
+    [SerializeField] float torpedoSoundVolume;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Fire();
+    }
+
+
+    private void Fire()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject torpedo = Instantiate(torpedoPrefab);
+            torpedo.transform.position = transform.position + transform.forward;
+            torpedo.transform.rotation = transform.rotation;
+            AudioSource.PlayClipAtPoint(torpedoSound, transform.position, torpedoSoundVolume);
+        }
+    }
+}
---
+++ StarTrek/Assets/Scripts/Torpedo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torpedo : MonoBehaviour
+{
+    public float torpedoSpeed = 8f;
+    public float lifeDuration = 5f;
+
+    private float lifeTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeTimer = lifeDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * torpedoSpeed * Time.deltaTime;
+        lifeTimer -= Time.deltaTime;
+
+        if (lifeTimer <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
