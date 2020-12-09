4e5efdc40566852be6c021e73f9b66dc06ab1202
Charles <c.wolfe132@gmail.com>
2020-12-09 07:51:26 -0800

    borg shoots now

---
+++ StarTrek/Assets/Scripts/BorgShooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorgShooting : MonoBehaviour
+{
+    [Header("Projectiles")]
+    [SerializeField] GameObject borgTorpedoPrefab;
+
+    [Header("Sound Effects")]
+    [SerializeField] AudioClip borgTorpedoSound;
+    [SerializeField] float borgTorpedoVolume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject torpedo = Instantiate(borgTorpedoPrefab);
+            torpedo.transform.position = transform.position + transform.forward;
+            torpedo.transform.rotation = transform.rotation;
+            AudioSource.PlayClipAtPoint(borgTorpedoSound, transform.position, borgTorpedoVolume);
+        }
+    }
+}
---
+++ StarTrek/Assets/Scripts/BorgTorpedo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorgTorpedo : MonoBehaviour
+{
+    [Header("Visual Effects")]
+    [SerializeField] GameObject hitVFX;
+    [SerializeField] float explosionDuration;
+
+    [Header("Sound Effects")]
+    [SerializeField] AudioClip hitSound;
+    [SerializeField] [Range(0, 1)] float hitSoundVolume = 0.7f;
+
+    public float torpedoSpeed = 8f;
+    public float lifeDuration = 5f;
+
+    private float lifeTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeTimer = lifeDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * torpedoSpeed * Time.deltaTime;
+        lifeTimer -= Time.deltaTime;
+
+        if (lifeTimer <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        GameObject boom = Instantiate(hitVFX, transform.position, Quaternion.identity) as GameObject;
+        Destroy(boom, explosionDuration);
+        AudioSource.PlayClipAtPoint(hitSound, transform.position, hitSoundVolume);
+    }
+}
