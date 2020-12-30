d4add995f201b455576f013880d673fba8e7abcb
Charles <c.wolfe132@gmail.com>
2020-12-30 06:57:42 -0800

    borg levels with player now

--- StarTrek/Assets/Scenes/BorgMotion.cs
+++ StarTrek/Assets/Scenes/BorgMotion.cs
@@ -6,6 +6,9 @@ public class BorgMotion : MonoBehaviour
 {
  
     [SerializeField] float rotateSpeed = 0.2f;
+    [SerializeField] GameObject ShipToFollow;
+    [SerializeField] float moveSpeedVertical = 10f;
+    float verticalRange = 23f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +21,23 @@ public class BorgMotion : MonoBehaviour
     void Update()
     {
         transform.Rotate(0f, rotateSpeed * Time.deltaTime, 0f);
+        LevelWithPlayer();
+    }
+
+    private void LevelWithPlayer()
+    {
+        float yDiff =  ShipToFollow.transform.position.y - transform.position.y;
+
+        if (Mathf.Abs(yDiff) < verticalRange) { return; }
+
+        if (yDiff >= verticalRange)
+        {
+            transform.Translate(0f, moveSpeedVertical * Time.deltaTime, 0f);
+        }
+
+        else if (yDiff <= -verticalRange)
+        {
+            transform.Translate(0f, -moveSpeedVertical * Time.deltaTime, 0f);
+        }
     }
 }
