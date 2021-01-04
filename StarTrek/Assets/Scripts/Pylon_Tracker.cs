18121300fb45dae4055fbc2439c12df9fff4fd90
Charles <c.wolfe132@gmail.com>
2021-01-03 19:04:20 -0800

    player collects pylons to reveal weak spot

---
+++ StarTrek/Assets/Scripts/Pylon_Tracker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pylon_Tracker : MonoBehaviour
+{
+    [SerializeField] GameObject Vulnerability;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckPylons();   
+    }
+
+    private void CheckPylons()
+    {
+        Debug.Log(GetComponentsInChildren<Pylon>().Length);
+
+        if (GetComponentsInChildren<Pylon>().Length <= 0)
+        {
+            Vulnerability.SetActive(true);  
+        }
+    }
+}
