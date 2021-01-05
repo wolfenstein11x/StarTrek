3f2e26e629aa6b64cb27f3a1908f5abcf752a8c8
Charles <c.wolfe132@gmail.com>
2021-01-04 17:05:40 -0800

    changed pylons borg prefab

--- StarTrek/Assets/Scripts/Pylon.cs
+++ StarTrek/Assets/Scripts/Pylon.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Pylon : MonoBehaviour
 {
+    [SerializeField] GameObject collectVFX;
+    [SerializeField] float explosionDuration;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +22,7 @@ public class Pylon : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        GameObject flash = Instantiate(collectVFX, transform.position, Quaternion.identity) as GameObject;
+        Destroy(flash, explosionDuration);
     }
 }
