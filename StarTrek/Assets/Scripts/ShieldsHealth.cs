84b34745e20ab0a8a92336d0bbab4c1c8280a9f7
Charles <cwolfe7@asu.edu>
2021-01-30 23:42:50 -0800

    shields appear when taking hit

--- StarTrek/Assets/Scripts/ShieldsHealth.cs
+++ StarTrek/Assets/Scripts/ShieldsHealth.cs
@@ -10,18 +10,39 @@ public class ShieldsHealth : MonoBehaviour
     [SerializeField] int startingShields = 100;
 
     public float shields;
+    private MeshRenderer shieldsMesh;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        shieldsMesh = GetComponent<MeshRenderer>();
+        HideShields();
+
         shieldsSlider.value = 1.0f;
 
         shields = startingShields;
     }
 
+    private void HideShields()
+    {
+        shieldsMesh.enabled = false;
+    }
+
+    IEnumerator ShowShields()
+    {
+        shieldsMesh.enabled = true;
+
+        yield return new WaitForSeconds(0.1f);
+
+        HideShields();
+    }
+   
+
     
     public void ProcessShieldsHit()    // give it an input parameter
     {
+        StartCoroutine(ShowShields());
         shields -= FindObjectOfType<BorgTorpedo>().damage;
         shieldsSlider.value = shields / startingShields;
 
